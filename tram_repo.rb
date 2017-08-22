07de443e542fb77acb20719fac85c6caea746d59
Tram Montano <tram.pham1989.tp@gmail.com>
2017-08-21 19:46:53 -0700

    gitHub notes

--- tram_repo.rb
+++ tram_repo.rb
@@ -1,1 +1,24 @@
 added notes 
+Creating a repository on git hub with a name that matches our computer’s
+Run commands from our terminal to transfer the local repository to the gitHub repository
+
+Commands:
+Git add . 
+Git commit -m “name_of_file”
+
+Only saves and updates items on your local repository, not the global directory.
+
+In order to update on gitHub you must you through the push steps.
+
+Commands:
+Git push origin master
+
+Git Cloning
+Clicking on the clone button
+copy the web URL
+Don’t clone inside an existing Git repository
+save in a folder that is not a Git repository
+Type in git clone in the terminal
+Paste the URL that was copied
+
+2. Clone Dani’s example_repo from earlier inside the Actualize folder 
